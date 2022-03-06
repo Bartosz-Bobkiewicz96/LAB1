6871e77f35002e39870f3e44becf40da81a15a3c
barto <bartosz.bobkiewicz1@gmail.com>
2022-03-06 16:18:59 +0100

    Laboratorium 1

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,24 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+public class Main {
+    public static void main (String[] args){
+        // miejsce pisania kodu
+        System.out.print("Ala\n");
+        System.out.print("ma\n");
+        System.out.print("kota\n");
+
+        System.out.println("Ala\n");
+        System.out.println("ma\n");
+        System.out.println("kota\n");
+
+        int a = 10;
+        double b = 5.21;
+        String s = "Wsei";
+
+
+        System.out.printf("a = %d, b = %f, s = %s %n", a,b,s);
+        System.out.printf("a = %d, b = %.2f, s = %20s %n", a,b,s);
+
+    }
+
+}
