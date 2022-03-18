a8c2d3d4de51a396485cc273b8cca1108f499579
barto <bartosz.bobkiewicz1@gmail.com>
2022-03-18 18:00:38 +0100

    Laboratorium 1

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,24 +1,32 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+
+
+import static java.lang.Math.log;
+
 public class Main {
-    public static void main (String[] args){
-        // miejsce pisania kodu
-        System.out.print("Ala\n");
-        System.out.print("ma\n");
-        System.out.print("kota\n");
+    static final String FORMAT = "%-10s%-10s%-10s%n";
 
-        System.out.println("Ala\n");
-        System.out.println("ma\n");
-        System.out.println("kota\n");
-
-        int a = 10;
-        double b = 5.21;
-        String s = "Wsei";
-
-
-        System.out.printf("a = %d, b = %f, s = %s %n", a,b,s);
-        System.out.printf("a = %d, b = %.2f, s = %20s %n", a,b,s);
-
+    public static void main(String[] args) {
+        System.out.printf(FORMAT, "arg", "log2", "pow");
+        for (int arg = 1, pow = 1; arg < 5; arg *= 2, pow++) {
+            print(arg, log2(arg), sumPowers(pow));
+        }
     }
 
+    static int log2(int arg) {
+        return (int) (log(arg) / log(2));
+    }
+
+    static int sumPowers(int arg) {
+        var sum = 0;
+        for (int i = 0; i < arg; i++) {
+            sum += Math.pow(2, i);
+        }
+        return sum;
+    }
+
+    static void print(int arg, int log2, int pow) {
+        System.out.printf(FORMAT, arg, log2, pow);
+    }
 }
