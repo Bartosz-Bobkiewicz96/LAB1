723c1e251b3e3f7a459ae440954be1dd64c2a0f5
barto <bartosz.bobkiewicz1@gmail.com>
2022-06-06 17:17:41 +0200

    Laboratorium 1

---
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/HexSys.java
@@ -0,0 +1,22 @@
+package pl.lublin.wsei.java.cwiczenia.mylib;
+
+import java.util.Scanner;
+
+public class HexSys {
+
+
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+
+        int num_1;
+
+        do {
+            System.out.printf("Prosze o podanie pierwszej liczby: ");
+            num_1 = input.nextInt();
+            if (num_1 == 0) break;
+
+            System.out.printf("Dzięstne liczba to: %d, Liczba dwójkowa:%s , liczba szesnastkowa: %s%n",num_1, Integer.toBinaryString(num_1),Integer.toHexString(num_1));
+
+        } while(true);
+    }
+}
